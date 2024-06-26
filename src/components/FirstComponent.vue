4b63ad88cbdba33f44a39d8b7b644a43380d566f
fanecovsf <gustavo_h07@hotmail.com>
2024-06-25 21:03:24 -0300

    initial commit

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Meu primeiro componente</h1>
+        <p>Componente {{name}}</p>
+        <p>Idade: {{age}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FirstComponent',
+        data() {
+            return {
+                name: 'Jeunesse',
+                age: 3
+            }
+        }
+    }
+</script>
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Meu nome é: {{ name }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LifeCycle',
+
+        data() {
+            return {
+                name: 'To em dúvida'
+            }
+        },
+
+        created() {
+            setTimeout(() => {
+                this.name = 'Gustavo'
+            }, 1000)
+        },
+
+        mounted() {
+            setTimeout(() => {
+                this.name = 'Felipe'
+            }, 2000)
+        }
+    }
+</script>
