e61491d771034bb0aa5b59638a3b751eca1dc6f3
fanecovsf <gustavo_h07@hotmail.com>
2024-06-27 07:17:11 -0300

    diretiva v-for

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,10 +2,13 @@
     <div>
         <p v-if="isWorking">Estou trabalhando</p>
         <p v-else>Não estou trabalhando atualmente</p>
-        <p>Utilizo as seguintes tecnologias:</p>
+        <p>Utilizo as seguintes tecnologias para backend:</p>
         <ul>
-            <li>Javascript</li>
-            <li>Node.Js</li>
+            <li v-for="(tec, index) in backendTecs" v-bind:key="index">{{ tec }}</li>
+        </ul>
+        <p>Utilizo as seguintes tecnologias para frontend:</p>
+        <ul>
+            <li v-for="tec in frontendTecs" v-bind:key="tec.id">{{ tec.name }}</li>
         </ul>
         <div>
             <button @click="toggleEmail">{{ toggleText }}</button>
@@ -24,7 +27,22 @@ export default {
             showEmail: true,
             email: 'teste@teste.com',
             portLink: 'https://google.com',
-            toggleText: 'Esconder email'
+            toggleText: 'Esconder email',
+            backendTecs: [
+                'Python',
+                'Java',
+                'PHP'
+            ],
+            frontendTecs: [
+                {
+                    "id": 1,
+                    "name": "Vue.Js",
+                },
+                {
+                    "id": 2,
+                    "name": "React.Js"
+                }
+            ]
         }
     },
 
