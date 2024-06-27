e12c1f2b66d838fa4f9df820413200431ef831f2
fanecovsf <gustavo_h07@hotmail.com>
2024-06-27 07:46:28 -0300

    multiplos eventos com métodos assíncronos

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -3,10 +3,13 @@
         <h1>Meu primeiro componente</h1>
         <p>Componente {{name}}</p>
         <p>Idade: {{age}}</p>
+        <MultipleEventsVue/>
     </div>
 </template>
 
 <script>
+    import MultipleEventsVue from './MultipleEvents.vue'
+
     export default {
         name: 'FirstComponent',
         data() {
@@ -14,6 +17,10 @@
                 name: 'Jeunesse',
                 age: 3
             }
+        },
+
+        components: {
+            MultipleEventsVue
         }
     }
 </script>
---
+++ src/components/MultipleEvents.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <button @click="first(txt1, $event), second(txt2, $event), third($event)">Ativar múltiplos eventos</button>
+        <p>{{ multipleEvents }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MultipleEvents',
+        data() {
+            return {
+                multipleEvents: "",
+                txt1: "Primeiro evento ativado!",
+                txt2: "Segundo evento ativado!"
+            }
+        },
+
+        methods: {
+            async first(txt, e) {
+                this.multipleEvents = txt
+            },
+
+            async second(txt, e) {
+                setTimeout(() => 
+                    this.multipleEvents = txt,
+                    5000
+                )
+            },
+
+            async third(e) {
+                setTimeout(() => this.multipleEvents = "", 8000)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        margin: 100px
+    }
+</style>
