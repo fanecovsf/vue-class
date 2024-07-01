139117f218c6e7c1bd42385cbd0e55a96a5bcb2a
fanecovsf <gustavo_h07@hotmail.com>
2024-07-01 06:31:39 -0300

    emit

---
+++ src/components/ChangePicture.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <button @click="$emit('changePicture')">Trocar imagem</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ChangePicture',
+
+    emits: ['changePicture']
+}
+</script>
+
--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,8 +1,11 @@
 <template>
     <img v-show="showDash" :src="dash" alt="">
+    <ChangePictureVue @changePicture="changePic"/>
 </template>
 
 <script>
+import ChangePictureVue from './ChangePicture'
+
 export default {
     // v-bind pode ser resumido apenas com 2 pontos
     // path iniciando com / sempre vai apontar para a pasta public do projeto
@@ -11,6 +14,20 @@ export default {
         return {
             dash: '/img/dash.png',
             showDash: true
+        }
+    },
+
+    components: {
+        ChangePictureVue
+    },
+
+    methods: {
+        changePic() {
+            if (this.dash == '/img/dash.png') {
+                this.dash = '/img/dash2.png'
+            } else {
+                this.dash = '/img/dash.png'
+            }
         }
     }
 }
